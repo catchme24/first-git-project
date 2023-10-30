bdc25ab5ad8ac6ffeb49fa9e5f1459d17ec8bb6a
catchme24 <goldmanhp11@gmail.com>
2023-10-30 12:26:07 +0400

    hey u r here

--- src/main/java/ru/demo/App.java
+++ src/main/java/ru/demo/App.java
@@ -7,7 +7,7 @@ package ru.demo;
 public class App 
 {
     public static void main( String[] args ) {
-        System.out.println("GIT DEVELOPSsss");
+        System.out.println("GIT DEVELOP");
         System.out.println("LEARN ???");
         System.out.println("DIFFICULT");
         System.out.println("HAPPY");
