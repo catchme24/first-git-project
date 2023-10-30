8d811d6fd6e5a80c12ad810944dbf9a382b8013a
Simon <149361954+catchme24@users.noreply.github.com>
2023-10-30 11:59:24 +0400

    Update App.java
    change sout in App class

--- src/main/java/ru/demo/App.java
+++ src/main/java/ru/demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args ) {
         System.out.println("GIT");
-        System.out.println("LEARN");
+        System.out.println("LEARN ???");
     }
 }
