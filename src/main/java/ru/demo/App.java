4f8a67835f44ada361db00f8df49e223137267c8
Simon <149361954+catchme24@users.noreply.github.com>
2023-10-30 12:01:01 +0400

    Update App.java
    added new sout

--- src/main/java/ru/demo/App.java
+++ src/main/java/ru/demo/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args ) {
         System.out.println("GIT");
         System.out.println("LEARN ???");
+        System.out.println("HAPPY");
     }
 }
