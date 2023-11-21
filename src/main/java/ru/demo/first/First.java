d916bbe94ad1bff4db91c27bf9cbcfdee450f3f1
catchme24 <goldmanhp11@gmail.com>
2023-11-21 13:07:36 +0400

    added 3 SOUT

--- src/main/java/ru/demo/first/First.java
+++ src/main/java/ru/demo/first/First.java
@@ -9,6 +9,6 @@ public class First
     public static void main( String[] args ) {
         System.out.println("MAKED BY FIRST");
         System.out.println("MAKED BY FIRST");
-
+        System.out.println("MAKED BY FIRST 3");
     }
 }
