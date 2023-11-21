d4d18626dddb5d348455251cd724e5c6a284ea7a
catchme24 <goldmanhp11@gmail.com>
2023-11-21 12:49:59 +0400

    added 2 new classes First and Second

--- src/main/java/ru/demo/App.java
+++ src/main/java/ru/demo/App.java
@@ -4,7 +4,7 @@ package ru.demo;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args ) {
         System.out.println("GIT DEVELOP");
---
+++ src/main/java/ru/demo/second/Second.java
@@ -0,0 +1,13 @@
+package ru.demo.second;
+
+/**
+ * Hello world!
+ *
+ */
+public class Second
+{
+    public static void main( String[] args ) {
+        System.out.println("MAKED BY SECOND");
+
+    }
+}
